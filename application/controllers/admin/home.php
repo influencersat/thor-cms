5373d2ede88667ba164a811433a597e6ae70868b
David Thor <dc.thor@thing5.com>
2012-07-20 20:23:48 +0000

    Added admin directory to controllers.

---
+++ application/controllers/admin/home.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+    /**
+     * Load the necessary libraries and models for the controller.
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('User_Model');
+    }
+
+	public function index() {
+            
+	}
+}
+
