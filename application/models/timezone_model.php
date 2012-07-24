d81277655f83b984b6bfe4bfdf53d7556a460448
David Thor <thor.d@husky.neu.edu>
2012-07-23 20:59:01 -0400

    Finished creation of valid timezone array.

--- application/models/timezone_model.php
+++ application/models/timezone_model.php
@@ -66,39 +66,68 @@ class Timezone_Model extends CI_Model {
         'Europe/Helsinki'       => "(GMT+02:00) Helsinki",
         'Europe/Istanbul'       => "(GMT+02:00) Istanbul",
         'Asia/Jerusalem'        => "(GMT+02:00) Jerusalem",
-        'Europe/Kiev'           =>  "(GMT+02:00) Kyiv",
+        'Europe/Kiev'           => "(GMT+02:00) Kyiv",
         'Europe/Minsk'          => "(GMT+02:00) Minsk",
         'Europe/Riga'           => "(GMT+02:00) Riga",
         'Europe/Sofia'          => "(GMT+02:00) Sofia",
         'Europe/Tallinn'        => "(GMT+02:00) Tallinn",
         'Europe/Vilnius'        => "(GMT+02:00) Vilnius",
-        
+        'Asia/Baghdad'          => "(GMT+03:00) Baghdad",
+        'Asia/Kuwait'           => "(GMT+03:00) Kuwait",
+        'Europe/Moscow'         => "(GMT+03:00) Moscow",
+        'Africa/Nairobi'        => "(GMT+03:00) Nairobi",
+        'Asia/Riyadh'           => "(GMT+03:00) Riyadh",
+        'Europe/Volgograd'      => "(GMT+03:00) Volgograd",
+        'Asia/Tehran'           => "(GMT+03:30) Tehran",
+        'Asia/Baku'             => "(GMT+04:00) Baku",
+        'Asia/Muscat'           => "(GMT+04:00) Muscat",
+        'Asia/Tbilisi'          => "(GMT+04:00) Tbilisi",
+        'Asia/Yerevan'          => "(GMT+04:00) Yerevan",
+        'Asia/Kabul'            => "(GMT+04:30) Kabul",
+        'Asia/Yekaterinburg'    => "(GMT+05:00) Ekaterinburg",
+        'Asia/Karachi'          => "(GMT+05:00) Karachi",
+        'Asia/Tashkent'         => "(GMT+05:00) Tashkent",
+        'Asia/Kolkata'          => "(GMT+05:30) Kolkata",
+        'Asia/Kathmandu'        => "(GMT+05:45) Kathmandu",
+        'Asia/Almaty'           => "(GMT+06:00) Almaty",
+        'Asia/Dhaka'            => "(GMT+06:00) Dhaka",
+        'Asia/Novosibirsk'      => "(GMT+06:00) Novosibirsk",
+        'Asia/Bangkok'          => "(GMT+07:00) Bangkok",
+        'Asia/Jakarta'          => "(GMT+07:00) Jakarta",
+        'Asia/Krasnoyarsk'      => "(GMT+07:00) Krasnoyarsk",
+        'Asia/Chongqing'        => "(GMT+08:00) Chongqing",
+        'Asia/Hong_Kong'        => "(GMT+08:00) Hong Kong",
+        'Asia/Irkutsk'          => "(GMT+08:00) Irkutsk",
+        'Asia/Kuala_Lumpur'     => "(GMT+08:00) Kuala Lumpur",
+        'Australia/Perth'       => "(GMT+08:00) Perth",
+        'Asia/Singapore'        => "(GMT+08:00) Singapore",
+        'Asia/Taipei'           => "(GMT+08:00) Taipei",
+        'Asia/Ulaanbaatar'      => "(GMT+08:00) Ulaan Bataar",
+        'Asia/Urumqi'           => "(GMT+08:00) Urumqi",
+        'Asia/Seoul'            => "(GMT+09:00) Seoul",
+        'Asia/Tokyo'            => "(GMT+09:00) Tokyo",
+        'Asia/Yakutsk'          => "(GMT+09:00) Yakutsk",
+        'Australia/Adelaide'    => "(GMT+09:30) Adelaide",
+        'Australia/Darwin'      => "(GMT+09:30) Darwin",
+        'Australia/Brisbane'    => "(GMT+10:00) Brisbane",
+        'Australia/Canberra'    => "(GMT+10:00) Canberra",
+        'Pacific/Guam'          => "(GMT+10:00) Guam",
+        'Australia/Hobart'      => "(GMT+10:00) Hobart",
+        'Australia/Melbourne'   => "(GMT+10:00) Melbourne",
+        'Pacific/Port_Moresby'  => "(GMT+10:00) Port Moresby",
+        'Australia/Sydney'      => "(GMT+10:00) Sydney",
+        'Asia/Vladivostok'      => "(GMT+10:00) Vladivostok",
+        'Asia/Magadan'          => "(GMT+11:00) Magadan",
+        'Pacific/Auckland'      => "(GMT+12:00) Auckland",
+        'Pacific/Fiji'          => "(GMT+12:00) Fiji",
+        'Asia/Kamchatka'        => "(GMT+12:00) Kamchatka"
     );
 
     /**
-     *
+     * Get a list of all possible timezones.
+     * @return array - Associative array of timezones and valid PHP identifiers.
      */
     public function getUTCList() {
-        $abbreviations = DateTimeZone::listAbbreviations();
-
-        $cities = array();
-        foreach ($abbreviations as $key => $zones) {
-            foreach ($zones as $id => $zone) {
-                if (preg_match('/^(America|Antartica|Arctic|Asia|Atlantic|Europe|Indian|Pacific)\//', $zone['timezone_id']) &&
-                    $zone['timezone_id']) 
-                {
-                    $cities[$zone['timezone_id']][] = $key;
-                }
-            }
-        }
-
-        foreach ($cities as $key => $value) {
-            $cities[$key] = join(', ', $value);
-        }
-
-        $cities = array_unique($cities);
-        ksort($cities);
-
-        return $cities;
+        return $zones;
     }
 }
