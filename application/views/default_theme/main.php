d33e7f88120f1c110747232c9ebcaea3dacf68c4
David Thor <thor.d@husky.neu.edu>
2012-07-29 13:40:06 -0400

    Previous theming changes didn't work. Trying new stuff.

--- application/views/default_theme/main.php
+++ application/views/default_theme/main.php
@@ -1,1 +1,3 @@
-<?php echo $this->getHeader(); ?>
+<?php $this->load->view('default_theme/header', array('page' => $page)); ?>
+
+<?php $this->load->view('default_theme/footer', array('page' => $page)); ?>
